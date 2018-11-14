9a913e0a03df02f516474fb22b9f2029c62d53aa
Yurii Krupa <yurii.krupa.dev@gmail.com>
2018-11-14 15:00:16 +0200

    Project upload

---
+++ RxTodo/View/ViewControllers/TodoTableView/TodoTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TodoTableViewCell.swift
+//  RxTodo
+//
+//  Created by Yurii Krupa on 11/9/18.
+//  Copyright © 2018 Yurii Krupa. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var tagsLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
