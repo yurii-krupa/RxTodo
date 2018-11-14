94c49372f905f6a62ddaf267a1f59079667a4417
Yurii Krupa <yurii.krupa.dev@gmail.com>
2018-11-14 17:33:19 +0200

    Formatting

--- RxTodo/View/ViewControllers/TodoTableView/TodoDetailedViewController.swift
+++ RxTodo/View/ViewControllers/TodoTableView/TodoDetailedViewController.swift
@@ -11,7 +11,7 @@ import RxSwift
 import RxCocoa
 
 class TodoDetailedViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UITextField!
     @IBOutlet weak var contentTextView: UITextView!
     
@@ -30,31 +30,39 @@ class TodoDetailedViewController: UIViewController {
         super.viewDidLoad()
         let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: nil, action: nil)
         self.navigationItem.rightBarButtonItem = saveButton
+        
         self.contentTextView.layer.borderWidth = 1/8
         self.contentTextView.layer.borderColor = self.titleLabel.layer.borderColor
         self.contentTextView.layer.cornerRadius = 5
         
-        self.titleLabel.rx.controlEvent([.editingDidEnd]).subscribe(onNext: { [unowned self] (_) in
-            self.todoItem?.title = self.titleLabel.text
-            
-        }).disposed(by: disposeBag)
+        self.titleLabel.rx
+            .controlEvent([.editingDidEnd])
+            .subscribe(onNext: {
+                [unowned self] (_) in
+                self.todoItem?.title = self.titleLabel.text
+                
+            }).disposed(by: disposeBag)
         
-        self.contentTextView.rx.didEndEditing.subscribe(onNext: {
-            [unowned self] _ in
-            self.todoItem?.content = self.contentTextView.text
-            self.todoItem?.tags = self.matches(for: "\\B(\\#[a-zA-Z]+\\b)(?!;)", in: self.contentTextView.text)
-        }).disposed(by: disposeBag)
+        self.contentTextView.rx
+            .didEndEditing
+            .subscribe(onNext: {
+                [unowned self] _ in
+                self.todoItem?.content = self.contentTextView.text
+                self.todoItem?.tags = self.matches(for: "\\B(\\#[a-zA-Z]+\\b)(?!;)", in: self.contentTextView.text)
+            }).disposed(by: disposeBag)
         
-        saveButton.rx.tap.subscribe(onNext: { [unowned self] (_) in
-            guard let todoModel = self.todoModel, let todoItem = self.todoItem else { return }
-            todoModel.addNewTodo(todoItem)
-            self.navigationController?.popViewController(animated: true)
-        }).disposed(by: disposeBag)
+        saveButton.rx
+            .tap
+            .subscribe(onNext: {
+                [unowned self] (_) in
+                guard let todoModel = self.todoModel, let todoItem = self.todoItem else { return }
+                todoModel.addNewTodo(todoItem)
+                self.navigationController?.popViewController(animated: true)
+            }).disposed(by: disposeBag)
     }
-
+    
     
     func matches(for regex: String, in text: String) -> [String] {
-        
         do {
             let regex = try NSRegularExpression(pattern: regex)
             let results = regex.matches(in: text,
